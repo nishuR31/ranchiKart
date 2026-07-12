c1255485788b038aa1576db717df3e5576d04681
nishur31 <nishangithub@gmail.com>
2026-07-12 22:25:27 +0530

    damnchexck

---
+++ backend/src/routes/public.ts
@@ -0,0 +1,15 @@
+import fastifyStatic from "@fastify/static";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import type { FastifyInstance } from "fastify";
+
+const __filename: string = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
+
+export default function publicRoutes(app: FastifyInstance) {
+    app.register(fastifyStatic, {
+        root: path.join(__dirname, "../public"),
+        prefix: "/public/",
+    });
+
+}
